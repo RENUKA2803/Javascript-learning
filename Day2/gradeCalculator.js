0fc2016881c30aa04fa229029651b8fb021f6ca4
Renuka Krishnan <renuka.k2022cce@sece.ac.in>
2026-08-19 15:58:16 +0530

    Add Day 2 JavaScript practice

---
+++ Day2/gradeCalculator.js
@@ -0,0 +1,16 @@
+let grade = 90;
+if(grade >=90){
+    console.log("A");
+}
+else if(grade>=75 && grade <=89){
+    console.log("B");
+}
+else if(grade>=60 && grade<=74){
+    console.log("C");
+}
+else if(grade>=50 && grade<=59){
+    console.log("D");
+}
+else{
+    console.log("F");
+}
