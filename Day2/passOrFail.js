63e2f3da8edb2a33717c83f39cc2c56f89de7dcf
Renuka Krishnan <renuka.k2022cce@sece.ac.in>
2026-08-19 15:02:10 +0530

    Add Day 2 JavaScript practice

---
+++ Day2/passOrFail.js
@@ -0,0 +1,7 @@
+let mark=21;
+if(mark>=50){
+    console.log("Pass");
+}
+else{
+    console.log("Fail")
+}
---
+++ Day2/positiveOrNegative.js
@@ -0,0 +1,7 @@
+let number = 23;
+if(number< 0){
+    console.log("Negative number");
+}
+else{
+    console.log("Positive number")
+}
